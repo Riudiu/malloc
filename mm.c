1895fab4aaebe7f92a615a2a8728913d2de68d3b
Riudiu <lacvert13@gmail.com>
2023-05-15 21:29:22 +0900

    mm_init, extend_heap 구현

--- mm.c
+++ mm.c
@@ -35,26 +35,129 @@ team_t team = {
     ""
 };
 
-/* single word (4) or double word (8) alignment */
-#define ALIGNMENT 8
+/// 가용 리스트 조작을 위한 기본 상수와 매크로 정의
+#define WSIZE 4       // Word and header/footer 크기(bytes)
+#define DSIZE 8       // Double word 크기 (btyes)
+#define CHUNKSIZE   (1 << 12)   // 초기 가용 블록과 힙 확장을 위한 기본 크기
 
-/* rounds up to the nearest multiple of ALIGNMENT */
-#define ALIGN(size) (((size) + (ALIGNMENT-1)) & ~0x7)
+#define MAX(x, y)   ((x) > (y) ? (x) : (y))    // x > y가 참이면 x, 거짓이면 y
 
+// PACK매크로 : 크기와 할당 비트를 통합해서 header와 footer에 저장할 수 있는 값 리턴
+#define PACK(size, alloc)   ((size) | (alloc))
 
-#define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
+// 주소에서 word 읽기 및 쓰기
+// unsigned int로 캐스팅하면 음의 정수값을 빼고 할당할 수 있음
+#define GET(p)  (*(unsigned int *)(p))        // 인자 p가 참조하는 워드를 읽어서 리턴
+#define PUT(p, val)  (*(unsigned int *)(p) = (val))  // 인자 p가 가리키는 워드에 val 저장
+
+// 주소 p에서 크기 및 할당된 필드 읽기
+#define GET_SIZE(p)    (GET(p) & ~0x7)  // header or footer의 사이즈 반환(8의 배수)
+#define GET_ALLOC(p)   (GET(p) & 0x1)   // 현재 블록 가용 여부 판단(0이면 alloc, 1이면 free)
+
+// bp(현재 블록의 포인터)로 현재 블록의 header 위치와 footer 위치 반환
+#define HDRP(bp)    ((char *)(bp) - WSIZE)
+#define FTRP(bp)    ((char *)(bp) + GET_SIZE(HDRP(bp)) - DSIZE)
+
+// 다음과 이전 블록의 포인터 반환
+#define NEXT_BLKP(bp)   (((char *)(bp) + GET_SIZE((char *)(bp) - WSIZE)))    // 다음 블록 bp 위치 반환(bp + 현재 블록의 크기)
+#define PREV_BLKP(bp)   (((char *)(bp) - GET_SIZE((char *)(bp) - DSIZE)))    // 이전 블록 bp 위치 반환(bp - 이전 블록의 크기)
+
+/// Declaration
+static void *heap_listp;
 
 /* 
  * mm_init - initialize the malloc package.
+ * 초기 가용 블록으로 힙 만들기
  */
 int mm_init(void)
-{
+{   
+    // 비어있는 힙 생성
+    if ((heap_listp = mem_sbrk(4*WSIZE)) == (void *)-1) {  
+        // heap_listp가 힙의 최댓값 이상을 요청한다면 fail
+        return -1;
+    }
+
+    PUT(heap_listp, 0);                             // 선형 padding
+    PUT(heap_listp + (1*WSIZE), PACK(DSIZE, 1));    // 프롤로그 header
+    PUT(heap_listp + (2*WSIZE), PACK(DSIZE, 1));    // 프롤로그 footer
+    PUT(heap_listp + (3*WSIZE), PACK(0, 1));        // 에필로그 header
+    heap_listp += (2*WSIZE);
+
+    // CHUNKSIZE 바이트의 사용 가능한 블록으로 빈 힙 확장
+    if (extend_heap(CHUNKSIZE/WSIZE) == NULL) {
+        return -1;
+    }
     return 0;
 }
 
+/*
+ * extend_heap 사용 경우
+ * 1) 힙이 초기화될 때
+ * 2) mm_malloc이 적당한 fit을 찾지 못했을 때
+ */
+static void *extend_heap(size_t words) 
+{
+    char *bp;
+    size_t size;
+
+    // 짝수 단어를 할당하여 정렬 상태 유지
+    size = (words % 2) ? (words + 1) * WSIZE : words * WSIZE; // words가 홀수면 +1을 해서 공간 할당
+    if ((long)(bp = mem_sbrk(size)) == -1) {
+        return NULL;
+    }
+
+    // 사용 가능한 블록 header/footer 및 epilogue header 초기화
+    PUT(HDRP(bp), PACK(size, 0));
+    PUT(FTRP(bp), PACK(size, 0));
+    PUT(HDRP(NEXT_BLKP(bp)), PACK(0, 1));
+    /*
+      extend_heap 블록 너머에 오지 않도록 배치한 블록 다음 공간을 블록이라 가정하고 epilogue header 배치
+      (실제로는 존재하지 않는 블록)
+    */
+
+    // 이전 블록이 사용 가능한 경우 병합
+    return coalesce(bp);   
+}
+
+static void *coalesce(void *bp) {
+    size_t prev_alloc = GET_ALLOC(FTRP(PREV_BLKP(bp)));
+    size_t next_alloc = GET_ALLOC(HDRP(NEXT_BLKP(bp)));
+    size_t size = GET_SIZE(HDRP(bp));
+
+    // case1: 앞, 뒤 블록 모두 할당되어 있을 때
+    if (prev_alloc && next_alloc) {
+        return bp;
+    }
+
+    // case2: 앞 블록 할당, 뒷 블록 가용
+    else if (prev_alloc && !next_alloc) {
+        size += GET_SIZE(HDRP(NEXT_BLKP(bp)));
+        PUT(HDRP(bp), PACK(size, 0));
+        PUT(FTRP(bp), PACK(size, 0));
+    }
+
+    // case3: 앞 블록 가용, 뒷 블록 할당
+    else if (!prev_alloc && next_alloc) {
+        size += GET_SIZE(HDRP(PREV_BLKP(bp)));
+        PUT(FTRP(bp), PACK(size, 0));
+        PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
+        bp = PREV_BLKP(bp);
+    }
+
+    // case4: 앞, 뒤 블록 모두 가용
+    else {
+        size += GET_SIZE(HDRP(PREV_BLKP(bp))) + GET_SIZE(FTRP(NEXT_BLKP(bp)));
+        PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
+        PUT(FTRP(NEXT_BLKP(bp)), PACK(size, 0));
+        bp = PREV_BLKP(bp);
+    }
+    return bp;
+} 
+
+
 /* 
- * mm_malloc - Allocate a block by incrementing the brk pointer.
- *     Always allocate a block whose size is a multiple of the alignment.
+ * mm_malloc - 새 가용 블록으로 힙 확장하기 
+ * brk 포인터를 증가시켜 블록을 할당, 항상 크기가 선형의 배수인 블록을 할당합니다.
  */
 void *mm_malloc(size_t size)
 {
